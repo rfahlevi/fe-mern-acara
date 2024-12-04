19e7f591b6f430e34e3b6c1e36b0f1031a4fd48e
Reza Fahlevi <rezafahlevi55@gmail.com>
2024-12-04 08:14:34 +0700

    feat: setup next auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import { cn } from "@/utils/cn";
 import { NextUIProvider } from "@nextui-org/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
@@ -21,18 +22,24 @@ const queryClient = new QueryClient({
   }
 })
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps }
+
+}: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <NextUIProvider>
-        <main className={cn(
-          "flex min-h-screen min-w-full items-center justify-center gap-10",
-          inter.className,
-        )}>
-          <Toaster position="top-right" richColors />
-          <Component {...pageProps} />
-        </main>
-      </NextUIProvider>
-    </QueryClientProvider>
+    <SessionProvider session={session}>
+      <QueryClientProvider client={queryClient}>
+        <NextUIProvider>
+          <main className={cn(
+            "flex min-h-screen min-w-full items-center justify-center gap-10",
+            inter.className,
+          )}>
+            <Toaster position="top-right" richColors />
+            <Component {...pageProps} />
+          </main>
+        </NextUIProvider>
+      </QueryClientProvider>
+    </SessionProvider>
   );
 }
